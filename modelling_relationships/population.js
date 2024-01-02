2d89aef53c0d637ff26894dc2891f395b5863c4d
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-01-02 19:33:22 +0530

    add: populate method

--- modelling_relationships/population.js
+++ modelling_relationships/population.js
@@ -1,24 +1,36 @@
-const mongoose = require('mongoose');
+const { ObjectId } = require("bson");
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/playground')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose
+  .connect("mongodb://localhost/mongoose")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
-const Author = mongoose.model('Author', new mongoose.Schema({
-  name: String,
-  bio: String,
-  website: String
-}));
+const Author = mongoose.model(
+  "Author",
+  new mongoose.Schema({
+    name: String,
+    bio: String,
+    website: String,
+  })
+);
 
-const Course = mongoose.model('Course', new mongoose.Schema({
-  name: String,
-}));
+const Course = mongoose.model(
+  "Course",
+  new mongoose.Schema({
+    name: String,
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Author",
+    },
+  })
+);
 
-async function createAuthor(name, bio, website) { 
+async function createAuthor(name, bio, website) {
   const author = new Author({
-    name, 
-    bio, 
-    website 
+    name,
+    bio,
+    website,
   });
 
   const result = await author.save();
@@ -27,23 +39,23 @@ async function createAuthor(name, bio, website) {
 
 async function createCourse(name, author) {
   const course = new Course({
-    name, 
-    author
-  }); 
-  
+    name,
+    author,
+  });
+
   const result = await course.save();
   console.log(result);
 }
 
-async function listCourses() { 
-  const courses = await Course
-    .find()
-    .select('name');
+async function listCourses() {
+  //* Populate given property   if it contains reference to other collenction and id of an object
+  //* params ('collection' ||collection[], 'select names of feilds')
+  const courses = await Course.find().populate("author", "name").select("name");
   console.log(courses);
 }
 
-createAuthor('Mosh', 'My bio', 'My Website');
+// createAuthor("Mosh", "My bio", "My Website");
 
-// createCourse('Node Course', 'authorId')
+// createCourse("Node Course", "6592f98b9f87186fcfd5cd84");
 
-// listCourses();
+listCourses();
